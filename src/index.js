3f8d9689808c42321ba3201e0691bcb488661555
Anna Mitina <anndan5810@gmail.com>
2021-10-04 03:40:27 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
 import './sass/main.scss';
+import 'material-icons/iconfont/material-icons.css';
+
+import refs from './js/refs.js';
+const { formSearch, imgGallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
+
+import BtnService from './js/btnService.js';
+import ApiService from './js/apiService.js';
+
+import imageCard from './templates/image-card.hbs';
+
+import { Notify } from 'notiflix';
+
+const imageService = new ApiService();
+const button = new BtnService({
+  loadMoreBtn: loadMoreBtn,
+  loadMoreLabel: loadMoreLabel,
+  loadMoreSpinner: loadMoreSpinner,
+  classList: 'd-none',
+});
+
+const fetchImg = () => {
+  button.disable();
+
+  imageService.fetchImg().then(hits => {
+    if (hits.length === 0) {
+        Notify.warning('Ошибка ввода')
+        return
+    }
+
+    imgGallery.insertAdjacentHTML('beforeend', imageCard(hits));
+
+    button.show();
+    button.enable();
+
+    imgGallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
+};
+
+const searchImg = event => {
+  event.preventDefault();
+  clearImgGallery();
+
+  const userRequest = event.currentTarget.elements.query.value.trim();
+  imageService.query = userRequest;
+
+  fetchImg();
+
+  formSearch.reset();
+};
+
+function clearImgGallery() {
+  imgGallery.innerHTML = '';
+}
+
+formSearch.addEventListener('submit', searchImg);
+loadMoreBtn.addEventListener('click', fetchImg);
